2d7a0503fbb3695e1612afd5bc76e071fd674b0f
Vaph05 <valeriedp05@gmail.com>
2022-12-06 21:10:32 -0500

    Create Gravedad.java

---
+++ app/src/main/java/com/example/hdp4proyectofinal/Gravedad.java
@@ -0,0 +1,14 @@
+package com.example.hdp4proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gravedad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gravedad);
+    }
+}
